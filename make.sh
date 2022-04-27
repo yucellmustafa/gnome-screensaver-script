2df21c72ce65ac88b394cbe29ffce6858afdc4dc
Mustafa Yucel <yucellmustafa@tuta.io>
2022-04-27 04:55:07 +0300

    fix make.sh

--- make.sh
+++ make.sh
@@ -2,13 +2,12 @@
 
 make=$1
 
-if [ $make == "install"]; then
+if [ $make = "install" ]; then
     sudo cp gscreensaver /usr/bin/
     sudo cp idle-time.py /usr/bin/
-elif [ $make == "uninstall"]; then
+elif [ $make = "uninstall" ]; then
     sudo rm /usr/bin/gscreensaver
     sudo rm /usr/bin/idle-time.py
-fi
 else
     echo "Usage: make.sh install|uninstall"
 fi
